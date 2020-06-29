6e77101a620101b48ecceb49af75b58336a261b5
Juan Esteban Muñoz <jemunozva@unal.edu.co>
2020-06-29 14:21:07 -0500

    Primera unificación

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { AuthGuardService } from './services/auth-guard.service';
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  { path:'', loadChildren: './pages/tabs/tabs.module#TabsPageModule'},
+  { path:'details', loadChildren: './pages/details/details.module#DetailsPageModule'},
+  {
+    path: 'tab2',
+    loadChildren: () => import('./pages/tab2/tab2.module').then( m => m.Tab2PageModule)
+  },
+  //{ path: 'mediciones', loadChildren: './pages/mediciones/mediciones.module#MedicionesPageModule'},
+  //{ path: 'medicion/:id', loadChildren: './pages/medicion/medicion.module#MedicionPageModule'},
+  //{ path: 'general', loadChildren: './pages/general/general.module#GeneralPageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
